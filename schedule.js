f8b2b25abe3021eb527f39d248d672914992f328
jjmoonisboss <jelyse.williams@csedge.org>
2020-08-15 20:08:43 +0000

    finally fixed the schedule page

--- schedule.js
+++ schedule.js
@@ -1,22 +1,22 @@
+
 // grabbing the calendar id from html
 const calendar = document.querySelector("#app-calendar ");
 
 // SO it can keep counting up the days
-
+console.log('hi');
 const isWeekend = day =>{
   return day === 5 && 6 && 12 && 13 && 19 && 20 && 26 && 27;
 }
 
 
 
-for(let day=1; day<= 30; day++){
+for(let day= 1; day<= 30; day++){
 
-  const weekend = isWeekend(day);
+  // const weekend = isWeekend(day);
+  
 
-  calendar.insertAdjacentHtml("beforeend",'<div class="day ${weekend }">${day}</div>' );
+  calendar.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`);
   // repeats for every day
-
-
 
 
 }
