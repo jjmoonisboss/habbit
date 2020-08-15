a643bdd506cacaa9424709cb655abb2e391fe16a
jjmoonisboss <jelyse.williams@csedge.org>
2020-08-15 19:19:21 +0000

    identifying weekend values and highlighting them

--- schedule.js
+++ schedule.js
@@ -2,7 +2,21 @@
 const calendar = document.querySelector("#app-calendar ");
 
 // SO it can keep counting up the days
+
+const isWeekend = day =>{
+  return day === 5 && 6 && 12 && 13 && 19 && 20 && 26 && 27;
+}
+
+
+
 for(let day=1; day<= 30; day++){
 
-  calendar.insertAdjacentHtml("beforeend",'<div class="day">${day}</div>' );
+  const weekend = isWeekend(day);
+
+  calendar.insertAdjacentHtml("beforeend",'<div class="day ${weekend }">${day}</div>' );
+  // repeats for every day
+
+
+
+
 }
