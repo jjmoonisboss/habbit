d9ffc9bdde822ee601254d9cfeb7b9dc81dd061c
jjmoonisboss <jelyse.williams@csedge.org>
2020-08-16 01:32:21 +0000

    fixing items, changing months of calendar

--- schedule.js
+++ schedule.js
@@ -4,7 +4,7 @@ const calendar = document.querySelector("#app-calendar ");
 
 const isWeekend = day =>{
   // 5 when its saturday and 6 when its sunday
- return day % 7 === 5 || day % 7 === 6;
+ return day % 7 === 3 || day % 7 === 4;
 }
 const isZero = day =>{
   return day === 0;
@@ -15,23 +15,25 @@ console.log('hi');
 
 // im trying to blank out the zero
 
-for(let day= 0; day<= 30; day++){
+for(let day= 0; day<= 31; day++){
 
   const weekend = isWeekend(day);
   const zero = isZero(day);
   
   const getDayName = day =>{
-    const date =  new Date(Date.UTC(2020, 10, day));
+    const date =  new Date(Date.UTC(2020, 12, day));
 
     return new Intl.DateTimeFormat("en-US" , {weekday: "short"})
     .format(date);
 }
 
+const dayName = getDayName(day);
+
 
 
   console.log(getDayName(day))
 
-  calendar.insertAdjacentHTML("beforeend",`<div class="day ${weekend ? "weekend" : ""} ${zero ? "zero" : ""}">${day}</div>`);
+  calendar.insertAdjacentHTML("beforeend",`<div class="day ${weekend ? "weekend" : ""} ${zero ? "zero" : ""}"><div class="name">${dayName}</div>${day}</div>`);
   // repeats for every day
 
 
